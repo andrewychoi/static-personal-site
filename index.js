5d4775e028094ca0ecdab376dd522c18516e8bb8
Andrew Choi <andrew@choi.com>
2018-09-03 11:31:33 -0400

    Add new option shifter

--- index.js
+++ index.js
@@ -1,25 +1,57 @@
 "use strict";
 
+function next_option() {
+	var current_shown = document.querySelectorAll(".show")[0];
+	var next_shown = current_shown.nextElementSibling;
+	if (next_shown === null) {
+		next_shown = document.querySelectorAll(".options li")[0];
+	}
+
+	current_shown.classList.add("hide");
+	current_shown.classList.remove("show");
+
+	next_shown.classList.remove("hide");
+	next_shown.classList.add("show");
+}
+
+function prev_option() {
+	var current_shown = document.querySelectorAll(".show")[0];
+	var next_shown = current_shown.previousElementSibling;
+	if (next_shown === null) {
+		var num_options = document.querySelectorAll(".options li").length;
+		next_shown = document.querySelectorAll(".options li")[num_options - 1];
+	}
+
+	current_shown.classList.add("hide");
+	current_shown.classList.remove("show");
+
+	next_shown.classList.remove("hide");
+	next_shown.classList.add("show");
+}
+
 (function() {
-	var list_els = document.querySelectorAll(".selector li");
-	Array.prototype.forEach.call(list_els, function(el, i){
+	// hide all options initially
+	var options = document.querySelectorAll(".options li");
+	Array.prototype.forEach.call(options, function(el, i){
 		el.classList.add("hide");
 	});
-	list_els[0].classList.remove("hide");
-	list_els[0].classList.add("show");
 
-	window.setInterval(function(options) {
-		var current_shown = document.querySelector(".show");
-		var next_shown = current_shown.nextElementSibling;
-		if (next_shown === null) {
-			next_shown = document.querySelectorAll(".selector li")[0];
-		}
+	// show the first one
+	options[0].classList.remove("hide");
+	options[0].classList.add("show");
 
-		current_shown.classList.remove("show");
-		current_shown.classList.add("hide");
+	// set a timer to choose the next option
+	var interval = window.setInterval(next_option, 3000);
 
-		next_shown.classList.remove("hide");
-		next_shown.classList.add("show");
-	}, 5000, list_els);
+	// allow shifting of selections
+	document.querySelector(".shifter.next").onclick = function() {
+		window.clearInterval(interval);
+		next_option();	
+	}
+	document.querySelector(".shifter.prev").onclick = function() {
+		window.clearInterval(interval);
+		prev_option();	
+	}
+
 
 })();
