6f762e3c34bd729390a2baaddf1d6258621c98cb
Andrew Choi <andrew@choi.com>
2018-11-28 23:28:00 -0500

    Clear out old site directory when rebuilding

--- update_site.sh
+++ update_site.sh
@@ -4,6 +4,8 @@ echo "Build new static pages..."
 cd jekyll
 JEKYLL_ENV=production bundle exec jekyll build
 
+echo "Clearing out old published site..."
+rm -r ../docs/writing
 echo "Copying to publishing directory..."
 cd _site
 cp -r . ../../docs/writing
