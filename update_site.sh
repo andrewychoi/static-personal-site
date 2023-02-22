8857fde7ea3bd4f41d3822c7cbdcfbd0458b340a
Andrew <andrew@choi.com>
2023-02-22 11:34:14 -0500

    Add captions to site

--- update_site.sh
+++ update_site.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
+set -e
 
 echo "Build new static pages..."
 cd jekyll
 JEKYLL_ENV=production bundle exec jekyll build
 
 echo "Clearing out old published site..."
-rm -r ../docs/writing
+rm -rf ../docs/writing
 echo "Copying to publishing directory..."
 cd _site
 cp -r . ../../docs/writing
-echo "Done"
+echo "Done!"
